1a2eadf2172a41143900d4716ba473f0bd9ee07e
nasr <156965421+div0rce@users.noreply.github.com>
2026-04-29 20:15:53 -0400

    firmware: centralize thread-safe application state (#8)

---
+++ include/app_state.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include "fault_state.h"
+
+bool app_state_init();
+void app_state_set(SystemState state);
+void app_state_set_fault(FaultCode fault);
+void app_state_clear_fault();
+SystemState app_state_get_state();
+FaultCode app_state_get_fault();
